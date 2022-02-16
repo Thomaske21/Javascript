747a6ccc140203051a4c8a3fc4cfceacd6c1cd39
Thomaske21 <thomas.verzelen@student.vives.be>
2022-02-16 18:39:59 +0100

    update

---
+++ JS2/opdracht 1/scripts/code.js
@@ -0,0 +1,8 @@
+const setup = () => {
+    window.alert("Dit is een mededeling.");
+    const confirm = window.confirm("Weet u het zeker?");
+    const prompt = window.prompt("Wat is uw naam?","onbekend");
+    console.log(confirm);
+    console.log(prompt);
+}
+window.addEventListener("load", setup);
--- JS2/opdracht arrays/scripts/code.js
+++ JS2/opdracht arrays/scripts/code.js
@@ -2,12 +2,12 @@ const setup = () => {
     const familieNamen = ['Frank','Axelle','Alex','Nick','Evelyne','Regine'];
     console.log(familieNamen.length);
     console.log(familieNamen[0],familieNamen[2],familieNamen[4]);
-function voegNaamToe(){
-    familieNamen.push(prompt());
-}
-voegNaamToe();
-console.log(familieNamen);
+    function voegNaamToe(){
+        familieNamen.push(prompt());
+    }
+    voegNaamToe();
+    console.log(familieNamen);
 
-console.log(familieNamen.join(', '));
+    console.log(familieNamen.join(', '));
 }
 window.addEventListener("load", setup);

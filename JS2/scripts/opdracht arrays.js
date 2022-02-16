440cb77e20fa9655dba2940cc6abcb74ddba6275
Thomaske21 <thomas.verzelen@student.vives.be>
2022-02-16 10:27:47 +0100

    update

---
+++ JS2/scripts/opdracht arrays.js
@@ -0,0 +1,13 @@
+const setup = () => {
+    const familieNamen = ['Frank','Axelle','Alex','Nick','Evelyne','Regine'];
+    console.log(familieNamen.length);
+    console.log(familieNamen[0],familieNamen[2],familieNamen[4]);
+function voegNaamToe(){
+    familieNamen.push(prompt());
+}
+voegNaamToe();
+console.log(familieNamen);
+
+console.log(familieNamen.join(', '));
+}
+window.addEventListener("load", setup);

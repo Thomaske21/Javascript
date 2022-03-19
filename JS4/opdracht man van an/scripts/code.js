79fb3c45d685f3d3f333da51a44ad0cdd1940092
Thomaske21 <thomas.verzelen@student.vives.be>
2022-03-19 10:29:13 +0100

    update

--- JS4/opdracht man van an/scripts/code.js
+++ JS4/opdracht man van an/scripts/code.js
@@ -1,6 +1,28 @@
 const setup = () => {
-    let tekst = document.getElementById("tekst").textContent;
-    console.log(tekst);
+    let tekst = document.getElementById("tekst").textContent.toLowerCase();
+    let i = 0;
+    let counter = 0;
+    let counter2 = 0;
+    while (i >= 0) {
+        if (tekst.indexOf("an", i) >= 0) {
+            counter++;
+            i = (tekst.indexOf("an", i) + 1);
+        } else {
+            i = -1;
+        }
+    }
+    console.log('an komt ' + counter+' keer voor');
+    /* Dit is met lastIndexof*/
+    let j = tekst.length;
+    while (j >= 0) {
+        if(tekst.lastIndexOf("an",j)>= 0){
+            counter2++;
+            j = tekst.lastIndexOf("an",j)-1;
+        } else {
+            j = -1;
+        }
+    }
+    console.log('an komt '+counter2+' keer voor');
 
 }
 window.addEventListener("load", setup);
